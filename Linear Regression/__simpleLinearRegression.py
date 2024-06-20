8149cb3166aac33cfa3ecdd27b5b43136171fb4d
VarunTyagarayanG <156878356+varunTyagarayanG@users.noreply.github.com>
2024-06-20 23:03:48 +0530

    Decision Tree Regression Added

--- Linear Regression/__simpleLinearRegression.py
+++ Linear Regression/__simpleLinearRegression.py
@@ -19,13 +19,13 @@ X_train , X_test , Y_train , Y_test = train_test_split(x , y , test_size=0.2, ra
 
 #Starts here!
 lr = LinearRegression()
-regressior = LinearRegression() 
-regressior.fit(X_train , Y_train)
+regressor = LinearRegression() 
+regressor.fit(X_train , Y_train)
 
-y_pred = regressior.predict(X_test)
+y_pred = regressor.predict(X_test)
 
 plt.scatter(X_train , Y_train , color ='red') 
-plt.plot(X_train , regressior.predict(X_train),color = 'blue')
+plt.plot(X_train , regressor.predict(X_train),color = 'blue')
 plt.title('salary Vs Experence 1')
 plt.xlabel('Years Of Exp')
 plt.ylabel('Salary')
@@ -33,7 +33,7 @@ plt.show()
 
 
 plt.scatter(X_test , Y_test , color ='red') 
-plt.plot(X_train , regressior.predict(X_train),color = 'blue')
+plt.plot(X_train , regressor.predict(X_train),color = 'blue')
 plt.title('salary Vs Experence 2')
 plt.xlabel('Years Of Exp')
 plt.ylabel('Salary')
